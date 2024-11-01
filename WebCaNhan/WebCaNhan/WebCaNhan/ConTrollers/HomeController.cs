47fcd29c92a01f6ec6805f47ca0e0cdef80ce8a2
viethung2004 <nguyenhung10204@gmail.com>
2024-11-01 21:21:30 +0700

    Add files via upload

---
+++ WebCaNhan/WebCaNhan/WebCaNhan/ConTrollers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCaNhan.ConTrollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebCaNhan/WebCaNhan/WebCaNhan/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Thêm dịch vụ MVC vào container
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Sử dụng các middleware để hỗ trợ routing
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
